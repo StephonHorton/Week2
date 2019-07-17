b264653ee7c1d9af1d2604c6b6558d62bd9de04b
StephonHorton <52015228+StephonHorton@users.noreply.github.com>
2019-07-17 11:19:58 -0400

    Add files via upload

--- TheDubs.java
+++ TheDubs.java
@@ -5,11 +5,16 @@ public class TheDubs {
 	public static void main(String[] args) {
 		Scanner code = new Scanner(System.in);
 		
-		int bye = 5;
+		int bye = 7;
 		int hi = 10;
 		int back = 0;
 		String name = "";
 		String color = "";
+		int neat =  9;
+		int meat = 2;
+		int neck = 3;
+		
+		
 		
 		System.out.println("Welcome to Dubs calculator");
 		System.out.println("Enter your name");
@@ -31,12 +36,24 @@ public class TheDubs {
 		System.out.println("The product of your two numbers is "+hi);
 		
 		hi = bye / back;
-		System.out.println("The quotient of your two numbers is "+hi);
+		System.out.println("The quotient of your two numbers is "+ hi);
 		System.out.println();
 		System.out.println("From my understanding,your name is " + name  );
 		System.out.println("and your favorite color is " + color  );
 		System.out.println();
-		System.out.println("Thank you for using our calculator");
+		System.out.println("Please rate your experience on a scale of 1 to 10. ");
+		neat = code.nextInt();
+		
+		 if (neat>= neck ){
+				System.out.println("Thank you for using our calculator");  
+		    } else if (neat< neck ){ System.out.println("We apologize for the inconvience " + name);
+			 
+		 System.out.println("Thank you for using our calculator"); 
+			 
+		 }
+		
+	
+		
 		
 	code.close();
 
