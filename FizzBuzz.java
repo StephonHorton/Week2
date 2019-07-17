e0241cf19ab0a2a79532af96b9fdca8b73858125
StephonHorton <52015228+StephonHorton@users.noreply.github.com>
2019-07-17 13:59:37 -0400

    Add files via upload

---
+++ FizzBuzz.java
@@ -0,0 +1,42 @@
+package day8;
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+		
+		int x = 0;
+		
+		for( x = 1 ; x<= 100; x++) {System.out.println(x);
+		
+		 
+		
+		
+		
+		
+		if (x % 3== 0 && x % 5 == 0) {System.out.println("FizzBuzz");}
+		else if ( x % 3 == 0) {
+			System.out.println("Fizz");
+		} else if(x % 5 == 0) {
+			System.out.println("Buzz");
+		} 
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		}
+	}
+}
+
